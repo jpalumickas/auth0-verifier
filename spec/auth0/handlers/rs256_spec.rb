f481e11be45f82ecef6091c35ee5a0f08df62a6a
Justas Palumickas <jpalumickas@gmail.com>
2019-01-10 22:45:59 +0200

    Dynamicly create certificate in tests

--- spec/auth0/handlers/rs256_spec.rb
+++ spec/auth0/handlers/rs256_spec.rb
@@ -12,61 +12,46 @@ RSpec.describe Auth0::Verifier::Handlers::Rs256 do
     config
   end
 
-  let(:public_key) do
-    "
-    -----BEGIN CERTIFICATE-----
-    MIICsDCCAhmgAwIBAgIJAP0uzO56NPNDMA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNV
-    BAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJbnRlcm5ldCBX
-    aWRnaXRzIFB0eSBMdGQwHhcNMTYwODAyMTIyMjMyWhcNMTYwOTAxMTIyMjMyWjBF
-    MQswCQYDVQQGEwJBVTETMBEGA1UECBMKU29tZS1TdGF0ZTEhMB8GA1UEChMYSW50
-    ZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKB
-    gQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvw
-    UAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03G
-    UnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQABo4GnMIGkMB0G
-    A1UdDgQWBBR7ZjPnt+i/E8VUy4tinxi0+H5vbTB1BgNVHSMEbjBsgBR7ZjPnt+i/
-    E8VUy4tinxi0+H5vbaFJpEcwRTELMAkGA1UEBhMCQVUxEzARBgNVBAgTClNvbWUt
-    U3RhdGUxITAfBgNVBAoTGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZIIJAP0uzO56
-    NPNDMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAnMA5ZAyEQgXrUl6J
-    T/JFcg6HGXj9yTy71EEMVp3Md3B8WwDvs+di4JFcq8FKSoGtTY4Pb5WE9QVUAmwE
-    sSQoETNYW3quRmYJCkpIHWnvUW/OAf2/Ejr6zXquhBC6WoCeKQuesMvo2qO1rStC
-    UWahUh2/RQt9XozEWPWJ9Oe6a7c=
-    -----END CERTIFICATE-----
-    "
-  end
   let(:private_key) do
-    "
-      -----BEGIN RSA PRIVATE KEY-----
-      MIICWwIBAAKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kV
-      w33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4
-      qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDA
-      QABAoGAD+onAtVye4ic7VR7V50DF9bOnwRwNXrARcDhq9LWNRrRGElESYYTQ6Eb
-      atXS3MCyjjX2eMhu/aF5YhXBwkppwxg+EOmXeh+MzL7Zh284OuPbkglAaGhV9bb
-      6/5CpuGb1esyPbYW+Ty2PC0GSZfIXkXs76jXAu9TOBvD0ybc2YlkCQQDywg2R/7
-      t3Q2OE2+yo382CLJdrlSLVROWKwb4tb2PjhY4XAwV8d1vy0RenxTB+K5Mu57uVS
-      THtrMK0GAtFr833AkEA6avx20OHo61Yela/4k5kQDtjEf1N0LfI+BcWZtxsS3jD
-      M3i1Hp0KSu5rsCPb8acJo5RO26gGVrfAsDcIXKC+bQJAZZ2XIpsitLyPpuiMOvB
-      bzPavd4gY6Z8KWrfYzJoI/Q9FuBo6rKwl4BFoToD7WIUS+hpkagwWiz+6zLoX1d
-      bOZwJACmH5fSSjAkLRi54PKJ8TFUeOP15h9sQzydI8zJU+upvDEKZsZc/UhT/Sy
-      SDOxQ4G/523Y0sz/OZtSWcol/UMgQJALesy++GdvoIDLfJX5GBQpuFgFenRiRDa
-      bxrE9MNUZ2aPFaFp+DyAe+b4nDwuJaW2LURbr8AEZga7oQj0uYxcYw==
-      -----END RSA PRIVATE KEY-----
-    "
+    OpenSSL::PKey::RSA.generate(2048)
   end
 
-  let(:private_key_object) do
-    OpenSSL::PKey::RSA.new(private_key.strip.split("\n").map(&:strip).join("\n"))
+  let(:public_key) do
+    private_key.public_key
   end
 
-  # let(:private_key) do
-  #   OpenSSL::PKey::RSA.generate(2048)
-  # end
-  #
-  # let(:public_key) do
-  #   private_key.public_key
-  # end
-  #
+  let(:certificate) do
+    cert_subject = '/C=LT/O=Test/OU=Test/CN=Test'
+
+    cert = OpenSSL::X509::Certificate.new
+    cert.subject = cert.issuer = OpenSSL::X509::Name.parse(cert_subject)
+    cert.not_before = Time.now
+    cert.not_after = Time.now + 365 * 24 * 60 * 60
+    cert.public_key = public_key
+    cert.serial = 0x0
+    cert.version = 2
+
+    ef = OpenSSL::X509::ExtensionFactory.new
+    ef.subject_certificate = cert
+    ef.issuer_certificate = cert
+    cert.extensions = [
+      ef.create_extension('basicConstraints', 'CA:TRUE', true),
+      ef.create_extension('subjectKeyIdentifier', 'hash')
+    ]
+    cert.add_extension(
+      ef.create_extension(
+        'authorityKeyIdentifier',
+        'keyid:always,issuer:always'
+      )
+    )
+
+    cert.sign private_key, OpenSSL::Digest::SHA1.new
+
+    cert.to_pem
+  end
+
   let(:x5c) do
-    public_key.to_s
+    certificate.to_s
       .gsub('-----BEGIN CERTIFICATE-----', '')
       .gsub('-----END CERTIFICATE-----', '')
       .split("\n").map(&:strip).join
@@ -88,17 +73,33 @@ RSpec.describe Auth0::Verifier::Handlers::Rs256 do
     }
   end
   let(:token) do
-    JWT.encode({ kid: 123, sub: 'john', aud: config.audience, iss: 'https://example.com/' }, private_key_object, 'RS256', kid: 123)
+    JWT.encode(
+      {
+        kid: 123,
+        sub: 'john',
+        aud: 'test-audience',
+        iss: 'https://example.com/'
+      },
+      private_key,
+      'RS256',
+      kid: 123
+    )
   end
 
   before do
-    allow_any_instance_of(Auth0::Verifier::Jwks).to receive(:data).and_return(jwks_data)
+    allow_any_instance_of(Auth0::Verifier::Jwks)
+      .to receive(:data).and_return(jwks_data)
   end
 
   it 'returns correct data' do
     expect(subject.verify).to eq(
       [
-        {"kid"=>123, "sub"=>"john", "aud"=>"test-audience", "iss"=>"https://example.com/"},
+        {
+          "kid"=>123,
+          "sub"=>"john",
+          "aud"=>"test-audience",
+          "iss"=>"https://example.com/"
+        },
         {"alg"=>"RS256", "kid"=>123}
       ]
     )
