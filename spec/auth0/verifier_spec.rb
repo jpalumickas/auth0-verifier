5d9093a32e117fb2a53b4936f6471253dda66d98
Justas Palumickas <jpalumickas@gmail.com>
2019-11-04 22:48:59 +0200

    Add more tests

--- spec/auth0/verifier_spec.rb
+++ spec/auth0/verifier_spec.rb
@@ -4,4 +4,42 @@ RSpec.describe Auth0::Verifier do
   it 'has a version number' do
     expect(Auth0::Verifier::VERSION).not_to be_nil
   end
+
+  describe '.verify!' do
+    let(:options) do
+      {
+        domain: 'example.com'
+      }
+    end
+
+    it 'raises an error invalid token' do
+      expect { described_class.verify!(options.merge(token: 'test')) }
+        .to raise_error(Auth0::Verifier::Error)
+    end
+  end
+
+  describe '.respond_to_missing?' do
+    it 'returns truthy value on verify method' do
+      expect(described_class).to respond_to(:verify)
+    end
+  end
+
+  describe '.verify' do
+    it 'raises an error invalid token' do
+      expect { described_class.verify('test') }
+        .to raise_error(Auth0::Verifier::Error)
+    end
+  end
+
+  describe '.configure' do
+    before do
+      described_class.configure do |config|
+        config.domain = 'https://test'
+      end
+    end
+
+    it 'has correct api key' do
+      expect(described_class.config.domain).to eq('test')
+    end
+  end
 end
